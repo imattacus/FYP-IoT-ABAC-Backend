81aa10162d9e26c0bdf2b41b9ebeda8926574683
mac523 <mac523@cs.bham.ac.uk>
2019-07-19 13:19:34 +0100

    react

---
+++ build/precache-manifest.cfbe1306f50f3f92ec6d2351c93a774b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ebef5d222d34732755061e951ec8ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "704b4b39e7f4e62c9dbb",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "53b2688a5d753e9185e9",
+    "url": "/static/css/main.bd66d727.chunk.css"
+  },
+  {
+    "revision": "704b4b39e7f4e62c9dbb",
+    "url": "/static/js/2.34663efd.chunk.js"
+  },
+  {
+    "revision": "53b2688a5d753e9185e9",
+    "url": "/static/js/main.bf0a04de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
